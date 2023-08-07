9cc535b9bede1924926f55df03be32f7d2072acf
Arkanisto <arkanisto@outlook.com>
2023-08-07 22:20:28 +0800

    issue fix

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark']
+extensions = ['recommonmark', 'sphinx_markdown_tables']
 
 templates_path = ['_templates']
 exclude_patterns = []
