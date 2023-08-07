cf1c47eda9f4524077ddd37747bd59d3c601a648
Arkanisto <arkanisto@outlook.com>
2023-08-07 22:33:10 +0800

    issue fix

--- source/conf.py
+++ source/conf.py
@@ -26,3 +26,6 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+extensions = ["myst_parser"]
+source_suffix = {'.rst': 'restructuredtext','.md': 'markdown',}
