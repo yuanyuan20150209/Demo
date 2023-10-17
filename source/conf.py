208d0459762e08bcf1e7e6dbc6659926397b6ebf
Arkanisto <arkanisto@outlook.com>
2023-10-17 20:08:46 +0800

    conf fix

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,10 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ['recommonmark']
+
+# extensions = ["myst-parser"]
+# source_suffix = {'.rst': 'restructuredtext','.md': 'markdown',}
 
 templates_path = ['_templates']
 exclude_patterns = []
